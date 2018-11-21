9175b40e378356b63a2ebd57d0de2e1fdfbf6426
Stephen French <stephenfrench9@gmail.com>
2018-11-21 00:05:08 -0700

    reformat how performance metrics are written to a file

--- src/play.py
+++ src/play.py
@@ -3,35 +3,21 @@ import numpy as np
 
 if __name__ == '__main__':
     train_labels = app.data()
-    modelOfInterest = "showers/"
+    modelOfInterest = "ants/"
     model = app.loadModel(modelOfInterest)
 
     # load test data
     batch_size = 30
-    start = 28000
-    test_generator = app.ImageSequence(train_labels=train_labels, batch_size=batch_size, start=start)
-    # x_test, y_act = test_generator.__getitem__(0);
-    # y_pred = model.predict(x_test)
-    #
-    # print(np.round(y_pred, 0))
-    #
-    # print(type(np.round(y_pred[-2][1], 0)))
-    # print(np.round(y_pred[-1][1], 1))
-    #
-    # a = np.zeros((2, 2))
-    #
-    # for i in range(y_pred.shape[0]):
-    #     prediction = int(np.round(y_pred[i][1], 0))
-    #     actual = int(np.round(y_act[i][1], 0))
-    #     a[prediction][actual] += 1
-    #
-    #
-    # notes = "train.csv " + str(batch_size) + str(start)
-    # app.writePerformance("showers/", a, notes)
-
+    valid_l = 30500; valid_h = 31000
+    test_generator = app.ImageSequence(train_labels=train_labels[valid_l:valid_h],
+                                       batch_size=batch_size,
+                                       start=valid_l)
+    # initialize confusion matrix
     a = np.zeros((2, 2))
+
+    # build confusion matrix
     num_batches = test_generator.__len__()
-    for batch in range(num_batches-1):
+    for batch in range(num_batches):
         x_test, y_act = test_generator.__getitem__(batch)
         y_pred = model.predict(x_test)
 
@@ -41,16 +27,14 @@ if __name__ == '__main__':
             actual = int(np.round(y_act[i][1], 0))
             a[prediction][actual] += 1
 
+    # calculate performance metrics
     class1 = a[0][1] + a[1][1]
     recall = a[1][1] / class1
-
     exclaim = a[1][0] + a[1][1]
     precision = a[1][1] / exclaim
 
-    print(a)
-    print("precision: " + str(precision))
-    print("recall: " + str(recall))
-    notes = ["train.csv ", str(batch_size), str(start)]
+    # save the results
+    notes = ["file: train.csv", "range: " + str(valid_l) + ":" + str(valid_h), "batch size: " + str(batch_size)]
     precision = ["precision: ", str(precision)]
     recall = ["recall: ", str(recall)]
     app.writePerformance(modelOfInterest, a, precision, recall, notes)
