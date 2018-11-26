e94c50fe88aa3d5d945d15cf8dbb4adb523b2960
Stephen French <stephenfrench9@gmail.com>
2018-11-26 09:15:51 -0800

    add a script to use as a sandbox

---
+++ src/sandbox.py
@@ -0,0 +1,14 @@
+import app
+
+if __name__ == '__main__':
+    print("huo")
+
+    train_labels = app.data()
+
+    seq = app.ImageSequence(train_labels=train_labels[0:100], batch_size=10, start=0)
+
+    x, y = seq.__getitem__(0)
+
+    print(x.shape)
+    print(y.shape)
+
